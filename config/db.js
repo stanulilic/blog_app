3d5f178a8231d31452814d94018a646fd75b253e
Stanley Ulili <stanleyulili@gmail.com>
2023-05-08 11:54:17 +0200

    integrate mongoose with express

---
+++ config/db.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+export default function connectDB() {
+  const url = "mongodb://127.0.0.1/blog_db";
+
+  try {
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+  const dbConnection = mongoose.connection;
+  dbConnection.once("open", (_) => {
+    console.log(`Database connected: ${url}`);
+  });
+
+  dbConnection.on("error", (err) => {
+    console.error(`connection error: ${err}`);
+  });
+  return;
+}
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import connectDB from "./config/db.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 app.get("/", (request, response) => {
   response.send({ message: "Hello from an Express API!" });
